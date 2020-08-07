d7d4749351c3290b9d4965d40466c66572e4e61f
shema102 <sergeishemshur@gmail.com>
2020-08-07 20:38:16 +0300

    Added setup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,17 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("SudokuSolver v0.1");
-
+        int[][] state;
+        state = new int[][]
+                {{0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 0, 0}};
+        Sudoku sudoku = new Sudoku(state);
     }
 }
