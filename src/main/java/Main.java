146347573fe4e46e58f961699271b0176d094385
shema102 <sergeishemshur@gmail.com>
2020-08-08 15:59:38 +0300

    Added input from keyboard

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,31 @@
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("SudokuSolver v0.1");
-        int[][] board;
-        board = new int[][]
-                {{0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                        {0, 0, 0, 0, 0, 0, 0, 0, 0}};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("SudokuSolver v0.1\n");
+        int[][] board = new int[9][9];
+        for (int row = 0; row < 9; row++) {
+            String buf;
+            buf = scanner.nextLine();
+            String[] nums = buf.split(" ");
+            for (int column = 0; column < 9; column++) {
+                board[row][column] = Integer.parseInt(nums[column]);
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.print("\n");
+        }
+
         Sudoku sudoku = new Sudoku(board);
+        sudoku.solve();
+        System.out.println(sudoku);
     }
 }
